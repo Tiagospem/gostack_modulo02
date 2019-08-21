8eec90c22eedc2e245cc9181cbc3f2b00580d5e9
Tiagospem <tiagospem@gmail.com>
2019-08-21 12:19:35 -0300

    feature: configurado mailtrap, instalacao do nodemailer e envio de email ao cancelar agendamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f12ebf48942d1b',
+    pass: 'f3eefc18fd62d6',
+  },
+  default: {
+    from: 'Tiago Padilha <noreply@gobarber.com>',
+  },
+};
